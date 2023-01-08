012c1aa2e36a15eab486ddd6ea0d91dc5840094e
ratul.sikder <ratul.sikder@rokomari.com>
2023-01-08 16:44:33 +0600

    Add Konva Library

--- src/app/features/designer/pages/studio/studio.component.ts
+++ src/app/features/designer/pages/studio/studio.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as Konva from '../../../../../assets/konva.min.js';
 
 @Component({
   selector: 'app-studio',
@@ -11,12 +12,35 @@ export class StudioComponent implements AfterViewInit {
 
   webContext: any;
 
+
   ngAfterViewInit(): void {
-    this.webContext = this.DrawingCanvas.nativeElement.getContext('2d');
+    this.webContext = new Konva.Stage({
+      container: 'container',   // id of container <div>
+      width: 500,
+      height: 500
+    });;
     
-    this.webContext.fillStyle = "green";
-    this.webContext.fillRect(10, 10, 150, 100);
-    
-  }  
+    let layer = new Konva.Layer();
+
+    // create our shape
+    let circle = new Konva.Circle({
+      x: this.webContext.width() / 2,
+      y: this.webContext.height() / 2,
+      radius: 70,
+      fill: 'red',
+      stroke: 'black',
+      strokeWidth: 4
+    });
+
+    // add the shape to the layer
+    layer.add(circle);
+
+    // add the layer to the stage
+    this.webContext.add(layer);
+
+    // draw the image
+    layer.draw();
+
+  }
 
 }
